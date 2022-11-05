1eb537780631355430e73682468437a920c21830
Edgar Nunez <nure.edgare@gmail.com>
2022-11-05 12:33:36 -0700

    Meaven Project
    Hello world check

---
+++ src/version2/src/main/java/spotilike/App.java
@@ -0,0 +1,13 @@
+package spotilike;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
